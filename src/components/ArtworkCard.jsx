1c04c7f0026113b185d263b07caf279bc2912993
Daniela Trinchera <trinchera.dm@gmail.com>
2022-09-14 09:53:31 +0200

    fixed file-structure

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -5,8 +5,10 @@ function ArtworkCard({ title, description, _id, owner, imageUrl }) {
     <div>
       <Link to={`/gallery/artwork/${_id}`}>
         <h3>{title}</h3>
-        <h4>By: {owner}</h4>
       </Link>
+      {/* // another link to owner's gallery */}
+      <h4>By: {owner}</h4>
+
       <p style={{ maxWidth: "400px" }}>{description} </p>
       <img src={imageUrl} alt="master-piece" width="200" />
     </div>
